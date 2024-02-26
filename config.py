5b7cd51597e0ea741fde674712e5d85cffced283
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-02-26 18:28:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6943973940:AAFHDfIULwfBtgrXybZ8_27554Ru7Vkyxdo")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6914434441:AAFLB8It4SjkF-qntGT2Kr1r51lEcmXYbn8")
     API_ID = int(os.environ["API_ID", 27803618]
     API_HASH = os.environ["API_HASH", "2cdaef9643189f6bd9c7b31a70257356"]
     AUTH_USERS = "745084259,1923238241 """
